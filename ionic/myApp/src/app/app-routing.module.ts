bfb53dfa5299c551d8175a1178b6d3fc88f98d09
kejl <kejunliang@126.com>
2019-10-10 00:14:15 +0800

    整理验证邮件到authemail目录并修改初是验证页面增加图标等

--- ionic/myApp/src/app/app-routing.module.ts
+++ ionic/myApp/src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '', redirectTo: '/authemail', pathMatch: 'full' },
   {
     path: 'tabs',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
@@ -11,7 +11,9 @@ const routes: Routes = [
   { path: 'account', loadChildren: './setup/account/account.module#AccountPageModule' },
   { path: 'password', loadChildren: './setup/password/password.module#PasswordPageModule' },
   { path: 'offline', loadChildren: './setup/offline/offline.module#OfflinePageModule' },
-  { path: 'language', loadChildren: './setup/language/language.module#LanguagePageModule' },  { path: 'loginpass', loadChildren: './login/loginpass/loginpass.module#LoginpassPageModule' },
+  { path: 'language', loadChildren: './setup/language/language.module#LanguagePageModule' },
+  { path: 'loginpass', loadChildren: './login/loginpass/loginpass.module#LoginpassPageModule' },
+  { path: 'authemail', loadChildren: './login/authemail/authemail.module#AuthemailPageModule' },
 
   
 
