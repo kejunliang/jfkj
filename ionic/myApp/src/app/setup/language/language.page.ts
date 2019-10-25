7d2743bdfb0a21037a700a96fab413fa548e57cc
renxuze <rxuze666@163.com>
2019-10-25 11:09:05 +0800

    '绑定language接口'

--- ionic/myApp/src/app/setup/language/language.page.ts
+++ ionic/myApp/src/app/setup/language/language.page.ts
@@ -1,49 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
-import {HttpClient,HttpHeaders} from '@angular/common/http'
+import {HttpClient,HttpHeaders} from '@angular/common/http';
+import { first } from 'rxjs/operators';
+import { LanguageService } from "../../services/setup/language.service";
 @Component({
   selector: 'app-language',
   templateUrl: './language.page.html',
   styleUrls: ['./language.page.scss'],
 })
 export class LanguagePage implements OnInit {
-  public langularArr:any=[
-      {lan:'Dutch',shorthand:'dut'},
-      {lan:'zh',shorthand:'zh'},
-      {lan:'English',shorthand:'en'},
-      {lan:'French',shorthand:'fre'},
-      {lan:'German',shorthand:'ger'},
-      {lan:'Lceland',shorthand:'lce'},
-      {lan:'Norwegian',shorthand:'nor'},
-      {lan:'Portuguese',shorthand:'por'},
-      {lan:'Spanish',shorthand:'spa'},
-    ];
-  public language:string='';
+  public user: string='zhen ding';
+  public pass:string='p@ssw0rd';
+  public langularArr:any=[];
+  public lan:string='';
   public name:string;
-
-  constructor(public translate :TranslateService,public http:HttpClient) { 
+  constructor(public translate :TranslateService,public http:HttpClient, public LanguageService:LanguageService) { 
     // this.translate.setDefaultLang('zh');
    
   }
 
   ngOnInit() {
-    let api='/sfv3/integrumws.nsf/xp_App.xsp/getAllForms?ver=v2&languageid';
-    const httpOption={
-        headers:new HttpHeaders({'Content-Type':'application/json'})
-    }
-    this.http.post(api,httpOption).subscribe(res=>{
-        console.log(res)
-    })
-     //获取当前设置的语言
-    let browerLang=this.translate.getDefaultLang();
-    console.log(browerLang)
-    this.language=browerLang;
+    // this.user=localStorage.getItem('user');
+    console.log(this.user);
+   
+    this.LanguageService.getAppTranslation('zhen ding','p@ssw0rd').pipe(first()).subscribe(
+      data => {
+        this.langularArr=data.Languages;
+         //获取当前设置的语言
+        let browerLang=this.translate.getDefaultLang();
+        this.lan=browerLang;
+      }
+    )
+    
   }
   radioCheck(item){
-    console.log(this.language)
-    console.log(item.shorthand)
-    this.translate.setDefaultLang(item.shorthand)
-    this.translate.use(item.shorthand)
+    console.log(this.lan)
+    console.log(item.SelectedLanguages)
+    this.translate.setDefaultLang(item.SelectedLanguages)
+    this.translate.use(item.SelectedLanguages)
   }
 
 }
