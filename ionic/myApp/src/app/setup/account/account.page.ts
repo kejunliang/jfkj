e819152942c8f4db81230f52832dc893d9ef2b04
renxuze <rxuze666@163.com>
2019-10-28 16:45:09 +0800

    'myAccount'

--- ionic/myApp/src/app/setup/account/account.page.ts
+++ ionic/myApp/src/app/setup/account/account.page.ts
@@ -1,23 +1,42 @@
 import { Component, OnInit } from '@angular/core';
-
+import {HttpClient,HttpHeaders} from '@angular/common/http';
+import { first } from 'rxjs/operators';
+import { AccountService } from "../../services/setup/account.service";
+import { Storage } from '@ionic/storage';
 @Component({
   selector: 'app-account',
   templateUrl: './account.page.html',
   styleUrls: ['./account.page.scss'],
 })
 export class AccountPage implements OnInit {
-  public account:any={
-    name:'xx',
-    company:'xxxx',
-    email:'xxxx',
-    language:'xx',
-    logout:'xx',
-    activated:'xx'
-  }
-  constructor() { }
-
-  ngOnInit() {
+  public accountData:any={
+  };
+  public user:string='';
+  public pass:string='';
+  public emali:string=''
+  constructor(public http:HttpClient,public account:AccountService,private storage:Storage) { 
     
   }
+
+  ngOnInit() {
+  this.initData()
+    
+  }
+ initData(){
+  this.storage.get("loginDetails").then(data=>{
+    this.user=data.username;
+    this.pass=data.password;
+    this.emali=data.emali;
+    console.log(this.pass,this.user)
+    this.account.getAccount(this.user,this.pass,this.emali).pipe(first()).subscribe(
+      data => {
+       
+        this.accountData=data;
+        console.log(this.accountData)
+      }
+    )
+  })
+  
+ }
 
 }
--- ionic/myApp/src/app/setup/language/language.page.ts
+++ ionic/myApp/src/app/setup/language/language.page.ts
@@ -3,18 +3,19 @@ import { TranslateService } from '@ngx-translate/core';
 import {HttpClient,HttpHeaders} from '@angular/common/http';
 import { first } from 'rxjs/operators';
 import { LanguageService } from "../../services/setup/language.service";
+import { Storage } from '@ionic/storage';
 @Component({
   selector: 'app-language',
   templateUrl: './language.page.html',
   styleUrls: ['./language.page.scss'],
 })
 export class LanguagePage implements OnInit {
-  public user: string='zhen ding';
-  public pass:string='p@ssw0rd';
+  public user: string='';
+  public pass:string='';
   public langularArr:any=[];
   public lan:string='';
   public name:string;
-  constructor(public translate :TranslateService,public http:HttpClient, public LanguageService:LanguageService) { 
+  constructor(public translate :TranslateService,public http:HttpClient, public LanguageService:LanguageService,private storage:Storage) { 
     // this.translate.setDefaultLang('zh');
    
   }
@@ -22,15 +23,20 @@ export class LanguagePage implements OnInit {
   ngOnInit() {
     // this.user=localStorage.getItem('user');
     console.log(this.user);
-   
-    this.LanguageService.getAppTranslation('zhen ding','p@ssw0rd').pipe(first()).subscribe(
-      data => {
-        this.langularArr=data.Languages;
-         //获取当前设置的语言
-        let browerLang=this.translate.getDefaultLang();
-        this.lan=browerLang;
-      }
-    )
+    this.storage.get("loginDetails").then(data=>{
+      console.log(data)
+      this.user=data.username;
+      this.pass=data.password;
+      this.LanguageService.getAppTranslation(this.user,this.pass).pipe(first()).subscribe(
+        data => {
+          this.langularArr=data.Languages;
+           //获取当前设置的语言
+          let browerLang=this.translate.getDefaultLang();
+          this.lan=browerLang;
+        }
+      )
+    })
+    
     
   }
   radioCheck(item){
