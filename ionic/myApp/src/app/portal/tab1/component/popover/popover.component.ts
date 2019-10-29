83c361750c7818115bba1f55f3c1dd9467cf84aa
kejl <kejunliang@126.com>
2019-10-29 23:26:23 +0800

    修改portal切换

--- ionic/myApp/src/app/portal/tab1/component/popover/popover.component.ts
+++ ionic/myApp/src/app/portal/tab1/component/popover/popover.component.ts
@@ -1,46 +1,55 @@
 import { Component, OnInit } from '@angular/core';
-import { PopoverController,NavParams } from '@ionic/angular';
+import { PopoverController, NavParams } from '@ionic/angular';
 import { NavController } from '@ionic/angular';
 import { TranslateService } from '@ngx-translate/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-popover',
   templateUrl: './popover.component.html',
   styleUrls: ['./popover.component.scss'],
 })
 export class PopoverComponent implements OnInit {
-  public setting:any=[];
-  public type:string;
-  constructor(public nav:NavController,public Popover:PopoverController,public translate:TranslateService,
-    public params:NavParams
-    ) { 
-    this.translate.get('setting').subscribe(res=>{
-      this.setting=res;
+  public setting: any = [];
+  public type: string;
+  public portalList: any = [];
+  constructor(public nav: NavController, public Popover: PopoverController, public translate: TranslateService,
+    public params: NavParams,
+    public router:Router
+  ) {
+    this.translate.get('setting').subscribe(res => {
+      this.setting = res;
     })
-    console.log(this.params.get("type")) 
-    this.type=this.params.get("type")
+    this.type = this.params.get("type")
+    if(this.params.get("portal")){
+      this.portalList = this.params.get("portal").items
+    }
+   
   }
- 
+
   ngOnInit() {
-   
+
   }
-  ngAfterViewInit(){
-   
-    let popover=document.querySelector('.popover-content');
-    popover['style'].width='23rem';
-    popover['style'].height='17rem'
+  ngAfterViewInit() {
+
+    let popover = document.querySelector('.popover-content');
+    popover['style'].width = '23rem';
+    popover['style'].height = '17rem'
   }
-  getLink(code){
-    if(code==1){
+  getLink(code) {
+    if (code == 1) {
       this.Popover.dismiss()
       this.nav.navigateBack('account');
-    }else if(code==2){
+    } else if (code == 2) {
       this.Popover.dismiss()
       this.nav.navigateBack('language');
-    }else if(code==3){
+    } else if (code == 3) {
       this.Popover.dismiss()
       this.nav.navigateBack('offline');
     }
+  }
+  getPortalLink(data){
+    this.router.navigate(['tabs/tab1'], {queryParams: {key:data}});
+    this.Popover.dismiss()
   }
 }
 
--- ionic/myApp/src/app/portal/tab1/tab1.page.ts
+++ ionic/myApp/src/app/portal/tab1/tab1.page.ts
@@ -4,42 +4,70 @@ import { PopoverComponent } from './component/popover/popover.component';
 import { NavController } from '@ionic/angular';
 import { Storage } from '@ionic/storage';
 import { StorageService } from '../../services/storage/storage.service';
-import {GetAppPortalService}  from '../../services/get-app-portal.service';
+import { GetAppPortalService } from '../../services/get-app-portal.service';
 import { first } from 'rxjs/operators';
+import { LoadingController } from '@ionic/angular';
+import { ActivatedRoute, Params } from '@angular/router';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
-  public name:string;
+  public name: string;
   public type: string;
+  public  portalTile:string;
+  public data = [
+    { "name": "测试1" },
+    { "name": "测试2" },
+    { "name": "测试3" },
+    { "name": "测试4" },
+    { "name": "测试5" },
+    { "name": "测试6" },
+    { "name": "测试7" },
+    { "name": "测试8" },
+    { "name": "测试9" },
+    { "name": "测试10" }
+  ];
+  public portalInfo: any;
+  public loading: any
+
   constructor(
     public popoverController: PopoverController,
-    public Nav:NavController,
-    private storage:Storage,
-    public storageService:StorageService,
-    public geapp:GetAppPortalService
+    public Nav: NavController,
+    private storage: Storage,
+    public storageService: StorageService,
+    public geapp: GetAppPortalService,
+    public loadingController: LoadingController,
+    public activeRoute: ActivatedRoute
+
   ) {
-    // this.translate.setDefaultLang('en');
-    this.storage.get("ous").then(data=>{
-      console.log("获取到ous"+data)
-    })
-    this.storage.get("loginDetails").then(data => {
-      console.log("loginDetails=="+JSON.stringify(data))
-      this.geapp.getPortalInfo(data.username,data.password).pipe(first())
-      .subscribe( data => {
-        console.log(data)
-      })
+    this.activeRoute.queryParams.subscribe(res => {
+      console.log(res);
+      this.portalTile=res.key
+      if(res.key){
+        console.log("laile")
+        
+        this.data.unshift({"name":res.key})
+      }
       
+    });
+    this.show()
+    this.storage.get("loginDetails").then(data => {
+      this.geapp.getPortalInfo(data.username, data.password).pipe(first())
+        .subscribe(data => {
+          this.portalInfo = data
+          this.portalTile=data.items[0].Title
+          this.hide()
+        })
+
     })
-   
   }
   async presentPopover(ev: any) {
     const popover = await this.popoverController.create({
       component: PopoverComponent,
       event: ev,
-      componentProps:{type:"setup"},
+      componentProps: { type: "setup" },
       translucent: true
     });
     return await popover.present();
@@ -48,17 +76,30 @@ export class Tab1Page {
     const popover = await this.popoverController.create({
       component: PopoverComponent,
       event: ev,
-      componentProps:{type:"portal"},
+      componentProps: { type: "portal", portal: this.portalInfo },
       translucent: true
     });
     return await popover.present();
   }
-  logout(){
-
+  logout() {
     this.Nav.navigateRoot('loginpass');
-    localStorage.setItem('hasLogged',"false");
+    localStorage.setItem('hasLogged', "false");
   }
-  getInfo(){
-    console.log(this.storageService.get('ous'))
+  getInfo() {
+
   }
+  async  show() {
+    this.loading = await this.loadingController.create({
+      message: 'loading....'
+    });
+    await this.loading.present();
+  }
+  async hide() {
+    if (this.loading) {
+      await this.loading.dismiss();
+    }
+  }
+
+
+
 }
