6722d1861bcee3225222a05c3dd5d99b56f3949a
kejl <kejunliang@126.com>
2019-11-12 14:46:31 +0800

    修改viewlist的标题

--- ionic/myApp/src/app/portal/form-list/form-list.page.ts
+++ ionic/myApp/src/app/portal/form-list/form-list.page.ts
@@ -16,6 +16,7 @@ export class FormListPage implements OnInit {
     "count":"",
     "curpage":""
   }
+  public vtitle:string ;
   constructor(
     private storage: Storage,
     public geapp: GetAppPortalService,
@@ -28,6 +29,7 @@ export class FormListPage implements OnInit {
         console.log("laile")
         console.log(res.vid)
         this.vid=res.vid.split("/")[1].split("?")[0]
+        this.vtitle=res.vtitle
         this.storage.get("loginDetails").then(data => {
           console.log(data)
           this.para.key=this.vid;
